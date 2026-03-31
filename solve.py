6b5a7d14ada9ab894ef25e8b7865fa243ceb75c5
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-03-31 12:02:54 +0500

    tasks2

--- solve.py
+++ solve.py
@@ -31,26 +31,45 @@ import random
 
 
 # Экспонента 
-x = np.array([1,2,3,4])
-y = np.exp(x/2)
+# x = np.array([1,2,3,4])
+# y = np.exp(x/2)
 
-polрешение задачy_lagrange = lagrange(x,y)
-poly_newton = KroghInterpolator(x,y)
+# poly_lagrange = lagrange(x,y)
+# poly_newton = KroghInterpolator(x,y)
 
-t = np.linspace(0,4,100)
+# t = np.linspace(0,4,100)
 
-plt.plot(t,poly_lagrange(t),'b-', label = "Лагранж")
-plt.plot(t,poly_newton(t),'r--', label = "Ньютон")
-plt.plot(x,y,'go',markersize = 8)
-plt.legend()
-plt.show()
+# plt.plot(t,poly_lagrange(t),'b-', label = "Лагранж")
+# plt.plot(t,poly_newton(t),'r--', label = "Ньютон")
+# plt.plot(x,y,'go',markersize = 8)
+# plt.legend()
+# plt.show()
 
 
 # # Ломанная
 # x = np.array([0,1,2,3,4,5,6])
 # y = np.array([1,5,2,9,3,8,4])
 
+# x_lin = np.linspace(0,9,100)
+# y_lin = np.interp(x_lin,x,y)
+
+# plt.plot(x_lin,y_lin,'b-',label = "Линейная интерполяция")
+# plt.plot(x,y,'ro', label = "Исходные точки")
+# plt.legend()
+# plt.grid(True)
+# plt.show()
 
 # # Затухание
 # x = np.array([0,1,2,3,4,5,6,7])
 # y = np.exp(-x/3)*np.cos(x**2)
+
+# poly_lagrange = lagrange(x,y)
+# poly_newton = KroghInterpolator(x,y)
+
+# t = np.linspace(0,7,100)
+
+# plt.plot(t,poly_lagrange(t),'b-', label = "Лагранж")
+# plt.plot(t,poly_newton(t),'r--', label = "Ньютон")
+# plt.plot(x,y,'go',markersize = 8)
+# plt.legend()
+# plt.show()
