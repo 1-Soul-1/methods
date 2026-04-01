2a98518e4e9c7aa8736994a0f547da71ba537bdd
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-04-01 12:09:26 +0500

    spline-and-image-processing

---
+++ methodimg.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import data, transform, io
+from skimage.util import img_as_float
+
+img = io.imread('./Tigers.png')
+
+# Уменьшаем картинку для дальнейшего увеличения и видимости разницы
+img_small = transform.resize(img,(50,50))
+
+img_nearest = transform.resize(img_small,(200,200), order = 0) # ближайший сосед
+img_linear = transform.resize(img_small,(200,200), order = 1) # линейная
+img_cubic = transform.resize(img_small,(200,200), order = 3) # кубический сплайн
+img_quintic = transform.resize(img_small,(200,200), order = 3) # сплайн 5-го порядка
+
+# Визуализация
+fig,axes = plt.subplots(2,3,figsize = (12,8))
+
+axes[0,0].imshow(img,cmap = 'gray')
+axes[0,0].set_title("Оригинал 512х512")
+axes[0,0].axis('off')
+
+axes[0,1].imshow(img_small,cmap = 'gray')
+axes[0,1].set_title("Уменьшенное 50х50")
+axes[0,1].axis('off')
+
+axes[0,2].imshow(img_linear,cmap = 'gray')
+axes[0,2].set_title("Ближайший соcед(order = 0)")
+axes[0,2].axis('off')
+
+axes[1,0].imshow(img_cubic,cmap = 'gray')
+axes[1,0].set_title("Линейная(order = 1)")
+axes[1,0].axis('off')
+
+axes[1,1].imshow(img_quintic,cmap = 'gray')
+axes[1,1].set_title("Кубическая")
+axes[1,1].axis('off')
+
+axes[1,2].imshow(img,cmap = 'gray')
+axes[1,2].set_title("Сплайн 5-го порядка")
+axes[1,2].axis('off')
+
+plt.tight_layout()
+plt.show()
---
+++ spline.py
@@ -0,0 +1,40 @@
+# Рассчёт сплайнов
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline, UnivariateSpline, PchipInterpolator
+
+# Простой кубический сплайн
+x = np.array([0,1,2,3,4,5])
+y = np.array([2,8,4,5,1,0])
+t = np.linspace(0,5,100)
+
+cs = CubicSpline(x,y)
+
+# plt.plot(t,cs(t))
+# plt.plot(x,y,'ro')
+# plt.show()
+
+
+# Сглаживающий сплайн
+x = np.array([0,1,2,3,4,5])
+y = np.array([2,8,4,5,1,0])
+t = np.linspace(0,5,100)
+
+spline = UnivariateSpline(x,y,s = 0.5) # s = 0 проходит черех точки
+
+# plt.plot(t,spline(t))
+# plt.plot(x,y,'ro')
+# plt.show()
+
+
+# Сплайн Эрмита
+x = np.array([0,1,2,3,4,5,5,7,3,2,6,7,8,9,3,2,4,5,8,1,1,0,1,8,5,6,3,6,4,0])
+y = np.array([2,8,4,5,1,0,1,0,0,0,1,3,0,5,4,3,2,7,4,1,5,7,1,1,2,6,4,4,6,3])
+t = np.linspace(0,30,100)
+
+phip = PchipInterpolator(x,y)
+
+plt.plot(t,phip(t))
+plt.plot(x,y,'ro')
+plt.show()
